1713e2bfb41fa5681859aa2730f7c4589262446c
quijano <safiro.quijano@nokia.com>
2020-04-21 13:49:27 +0800

    added /datadrop route for getting download links of files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getFolderID,getFoldersList} = require('./googledrive/googleDriveClient.js');
+const {getFolderWithDate,getFoldersList,getDownloadLink} = require('./googledrive/googleDriveClient.js');
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -14,6 +14,12 @@ app.get('/',(req,res)=>{
  *               format is YYYYMMDD.
  *               example usage: '/listfolders?date=20200420'
  *      [empty query]: lists all folders under root DOH data drop drive folder
+ *  Returns a JSON object with parameters "id" (folder id) and "name"
+ *      ex. 
+ *          [{
+ *                  "id": "1zgEKsGhx-5VIz3lHdThWcSDlmo3NI7Gr",
+ *                  "name": "DOH COVID Data Drop_ 20200420"
+ *          }]
 **/
 app.get('/listfolders',(req,res)=>{
     const date = req.query.date; //YYYYMMDD
@@ -23,11 +29,22 @@ app.get('/listfolders',(req,res)=>{
             res.send(data);
         });
     }
-    return getFolderID(date,(error,data)=>{
+    return getFolderWithDate(date,(error,data)=>{
         if(error) return res.send(error);
         res.send(data);
     });
 });
+
+app.get('/datadrop/:date/:type',(req,res)=>{
+    getDownloadLink(req.params.date,req.params.type,(error,data)=>{
+        if(error) return res.send(error);
+        res.send(data);
+    });
+});
+
+app.get('*',(req,res)=>{
+    res.send(`cannot resolve ${req.path}`);
+})
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
